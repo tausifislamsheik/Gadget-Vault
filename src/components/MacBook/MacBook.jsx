86b3a250ff708584ff7b556ae588d75b96c7c51d
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-08-17 11:27:55 +0600

    Macbook section added

---
+++ src/components/MacBook/MacBook.jsx
@@ -0,0 +1,24 @@
+const MacBook = ({macBook}) => {
+
+    const {image,name,price} = macBook;
+
+    return (
+        <div className="card p-5 h-96 border border-gray-300">
+            <figure className="bg-gray-50 p-5">
+                <img
+                className="h-40"
+                src={image}
+                alt="Gadgets" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title text-xl">{name}</h2>
+                <p className="text-gray-500 font-semibold">Price: $ {price}</p>
+                <div className="card-actions justify-end">
+                <button className="btn text-[#9538E2] hover:bg-[#9538E2] hover:text-white mt-6 font-semibold bg-white p-2 px-4 rounded-full border-[#9538E2]">View Details</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MacBook;
--- src/components/MacBooks/MacBooks.jsx
+++ src/components/MacBooks/MacBooks.jsx
@@ -1,9 +1,24 @@
-
+import { useEffect, useState } from "react";
+import MacBook from "../MacBook/MacBook";
 
 const MacBooks = () => {
+
+    const [macBooks, setMacBooks] = useState([]);
+
+    useEffect(() =>{
+        fetch('allProducts.json')
+        .then(res => res.json())
+        .then(data => setMacBooks(data))
+    },[])
+
+    const allMacBooks = macBooks.filter(macBook => macBook.category === "MacBook")
+
     return (
-        <div>
-            <h1>Macbook</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 ml-10">
+            
+            {
+                allMacBooks.map(macBook => <MacBook key={macBook.id} macBook={macBook}></MacBook>)
+            }
         </div>
     );
 };
