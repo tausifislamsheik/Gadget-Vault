fccfe24fb7ae16ec95967d389d5881981dae95a4
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-08-17 10:53:51 +0600

    Smart watches section added

--- src/components/SmartWatches/SmartWatches.jsx
+++ src/components/SmartWatches/SmartWatches.jsx
@@ -1,9 +1,22 @@
-
+import { useEffect, useState } from "react";
+import SmartWatch from "../SmartWatch/SmartWatch";
 
 const SmartWatches = () => {
+
+    const [smartWatches, setSmartWatches] = useState([]);
+
+    useEffect(() =>{
+        fetch('allProducts.json')
+        .then(res => res.json())
+        .then(data => setSmartWatches(data))
+    },[])
+
+    let allSmartWatches = smartWatches.filter(smartWatch => smartWatch.category === "Smartwatch")
     return (
-        <div>
-            <h1>Smart Watches</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 ml-10">
+            {
+                allSmartWatches.map(smartWatch => <SmartWatch key={smartWatch.id} smartWatch={smartWatch}></SmartWatch>)
+            }
         </div>
     );
 };
