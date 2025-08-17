b36a577594f43b3eabae0657c1ff74af94dc7977
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-08-17 11:44:43 +0600

    Footer section added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,36 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="text-center bg-neutral text-neutral-content py-10">
+                <h1 className="text-3xl font-bold mb-3">Gadget Vault</h1>
+                <p className="text-gray-500">Leading the way in cutting-edge technology and innovation.</p>
+            </footer>
+            <hr className=" text-gray-600" />
+            <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+                <h1></h1>
+                <nav>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const Root = () => {
     return (
-        <div className="sora ">
+        <div className="sora">
             <div className="bg-[#9538e2] text-white mx-4 lg:mx-12 mt-10 lg:px-20 rounded-t-2xl">
                 <Navbar></Navbar>
             </div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
