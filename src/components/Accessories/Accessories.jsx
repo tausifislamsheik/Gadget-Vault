bcf4a769d360630399a2b61be26e23c558110c16
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-08-17 11:34:16 +0600

    Accessories section added

--- src/components/Accessories/Accessories.jsx
+++ src/components/Accessories/Accessories.jsx
@@ -1,9 +1,22 @@
-
+import { useEffect, useState } from "react";
+import Accessory from "../Accessory/Accessory";
 
 const Accessories = () => {
+
+    const [accessories, setAccessories] = useState([]);
+
+    useEffect(() =>{
+        fetch('allProducts.json')
+        .then(res => res.json())
+        .then(data => setAccessories(data))
+    },[])
+
+    const allAccessories = accessories.filter(accessory => accessory.category === "Accessory")
     return (
-        <div>
-            <h1>Accessories</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 ml-10">
+            {
+                allAccessories.map(accessory => <Accessory key={accessory.id} accessory={accessory}></Accessory>)
+            }
         </div>
     );
 };
