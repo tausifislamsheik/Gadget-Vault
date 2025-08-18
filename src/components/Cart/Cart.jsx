b08c76e903ddcab041a46170f775819afedbf883
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-08-19 00:20:17 +0600

    Card style edited

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,26 +4,22 @@ const Cart = ({cart}) => {
     const {image, name, price, features} = cart;
     return (
         <div className="mx-10 lg:mx-40 mb-5">
-            <div className="hero bg-base-200 justify-start rounded-2xl">
-                <div className="hero-content flex-col lg:flex-row">
+            <div className="hero flex lg:justify-between bg-base-200 rounded-2xl">
+                <div className="hero-content lg:flex flex-col lg:flex-row">
                     <img
                     src={image}
                     className="w-52 h-56 object-cover rounded-lg bg-gray-200 p-5"
                     />
-                    <div className="flex">
-                         <div>
+                    <div className="">
                     <h1 className="text-3xl font-bold">{name}</h1>
                     <p className="font-bold py-1">Price: $ {price}</p>
                     {
                         features.map(feature => <li className="text-gray-400">{feature}</li>)
                     }
                     </div>
-                    <div className="justify-end">
-                        <MdDeleteForever  />
-                    </div>
-                    </div>
-                    
-                
+                </div>
+                <div className="mr-16">
+                        <MdDeleteForever className="text-3xl text-red-600 hover:text-red-700 cursor-pointer" />
                 </div>
             </div>
         </div>
