ae90473c9a387341844078e812cf1af33bdaa117
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-08-18 23:14:34 +0600

    Responsive added in the cart section

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,14 @@
 
 
 const Cart = ({cart}) => {
-   console.log(cart)
     const {image, name, price, features} = cart;
     return (
-        <div className="mx-40 mb-5">
+        <div className="mx-10 lg:mx-40 mb-5">
             <div className="hero bg-base-200 justify-start rounded-2xl">
                 <div className="hero-content flex-col lg:flex-row">
                     <img
                     src={image}
-                    className="w-40 rounded-lg bg-gray-200 p-5"
+                    className="w-52 h-56 object-cover rounded-lg bg-gray-200 p-5"
                     />
                     <div>
                     <h1 className="text-3xl font-bold">{name}</h1>
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -31,12 +31,12 @@ const Dashboard = () => {
                      </NavLink>
                 </div>
             </div> 
-            <div className="flex items-center justify-around my-10">
-                <h1 className="text-3xl font-bold">Cart</h1>
+            <div className="lg:flex items-center mx-4 justify-around my-10">
+                <h1 className="text-2xl lg:text-3xl font-bold">Cart</h1>
                 <div className="flex items-center gap-5">
-                    <p className="text-xl font-semibold">Total cost: $ <span className="font-bold">10000</span></p>
-                    <button className="btn btn-outline text-[#9538E2] px-6 font-semibold p-2 rounded-full">Sort by Price <BiSolidSortAlt className="text-lg" /></button>
-                    <button className="btn btn-outline text-white bg-[#9538E2] px-10 font-semibold p-2 rounded-full">Purchase</button>
+                    <p className="lg:text-xl font-semibold">Total cost: $ <span className="font-bold">10000</span></p>
+                    <button className="btn btn-outline text-[#9538E2] lg:px-6 font-semibold p-2 rounded-full">Sort by Price <BiSolidSortAlt className="text-lg" /></button>
+                    <button className="btn btn-outline text-white bg-[#9538E2] lg:px-10 font-semibold p-2 rounded-full">Purchase</button>
                 </div>
             </div>
                 {
