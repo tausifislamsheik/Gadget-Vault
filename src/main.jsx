a577e9dde00e662e534f00df6fc4952bb31ae6a5
Tausif Islam Sheik <mr.tausif904@gmail.com>
2025-08-29 12:26:59 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {
   createBrowserRouter,
-  Navigate,
   RouterProvider,
 } from "react-router-dom";
 import Root from './components/Root/Root';
@@ -33,10 +32,6 @@ const router = createBrowserRouter([
         path:'/',
         element:<Home></Home>,
         children:[
-          {
-            path:'',
-            element:<Navigate to={'/allProducts/1'}></Navigate>
-          },
           {
             path:'/allProducts',
             element:<AllProducts></AllProducts>
